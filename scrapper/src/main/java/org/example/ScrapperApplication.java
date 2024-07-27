7d1d682dd5acc7870f5a573191389495edcce0d4
AlexVolkov <AlexVolkovProgrammer@yandex.com>
2024-07-28 01:37:55 +0300

    addDockerCompose

--- scrapper/src/main/java/org/example/ScrapperApplication.java
+++ scrapper/src/main/java/org/example/ScrapperApplication.java
@@ -1,7 +1,6 @@
 package org.example;
 
 
-import org.example.service.github.GithubRepoHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
